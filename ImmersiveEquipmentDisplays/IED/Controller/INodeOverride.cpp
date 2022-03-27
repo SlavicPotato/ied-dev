SlavicPotato/ied-dev

commit 2ed4c70c6ace3531836403b830576c6065ce0b0b

Added ammo category to equipment form browser (filters/preferred items) Custom: optionally load a random model when additional items are specified

ImmersiveEquipmentDisplays/IED/Controller/INodeOverride.cpp
@@ -352,10 +352,10 @@ namespace IED
 				}
 
 				std::uint32_t result = 0;
-				std::uint32_t min = a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchAll) &&
-				                            !a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchCategoryOperOR) ?
-                                        2u :
-                                        1u;
+				std::uint32_t min    = a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchAll) &&
+                                            !a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchCategoryOperOR) ?
+				                           2u :
+                                           1u;
 
 				if (a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchSlots))
 				{
@@ -389,10 +389,10 @@ namespace IED
 				}
 
 				std::uint32_t result = 0;
-				std::uint32_t min = a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchAll) &&
-				                            !a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchCategoryOperOR) ?
-                                        2u :
-                                        1u;
+				std::uint32_t min    = a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchAll) &&
+                                            !a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchCategoryOperOR) ?
+				                           2u :
+                                           1u;
 
 				if (a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchSlots))
 				{
@@ -425,10 +425,10 @@ namespace IED
 				}
 
 				std::uint32_t result = 0;
-				std::uint32_t min = a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchAll) &&
-				                            !a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchCategoryOperOR) ?
-                                        2u :
-                                        1u;
+				std::uint32_t min    = a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchAll) &&
+                                            !a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchCategoryOperOR) ?
+				                           2u :
+                                           1u;
 
 				if (a_data.flags.test(Data::NodeOverrideConditionFlags::kMatchSlots))
 				{
@@ -459,7 +459,7 @@ namespace IED
 						if (a_form->IsArmor())
 						{
 							auto data = a_params.get_item_data();
-							auto it = data->find(a_form->formID);
+							auto it   = data->find(a_form->formID);
 							if (it != data->end())
 							{
 								it->second.matched = true;
@@ -814,24 +814,26 @@ namespace IED
 
 		for (auto& e : a_data)
 		{
-			if (e.offsetFlags.test(Data::NodeOverrideOffsetFlags::kIsGroup))
+			if (run_matches(e, a_params))
 			{
-				if (process_offsets(e.group, a_out, a_posAccum, a_params))
+				if (e.offsetFlags.test(Data::NodeOverrideOffsetFlags::kIsGroup))
+				{
+					if (process_offsets(e.group, a_out, a_posAccum, a_params))
+					{
+						matched = true;
+
+						if (!e.offsetFlags.test(Data::NodeOverrideOffsetFlags::kContinue))
+						{
+							break;
+						}
+					}
+				}
+				else
 				{
 					matched = true;
 
-					if (!e.offsetFlags.test(Data::NodeOverrideOffsetFlags::kContinue))
-					{
-						break;
-					}
-				}
-			}
-			else
-			{
-				a_params.clear_matched_items();
+					a_params.clear_matched_items();
 
-				if (run_matches(e, a_params))
-				{
 					if (e.offsetFlags.test_any(Data::NodeOverrideOffsetFlags::kAdjustFlags))
 					{
 						if (!(e.offsetFlags.test(Data::NodeOverrideOffsetFlags::kAdjustIgnoreDead) && a_params.get_actor_dead()))
@@ -867,8 +869,6 @@ namespace IED
 					{
 						a_posAccum = {};
 					}
-
-					matched = true;
 
 					if (!e.offsetFlags.test(Data::NodeOverrideOffsetFlags::kContinue))
 					{
@@ -909,7 +909,7 @@ namespace IED
 			{
 				ITaskPool::AddPriorityTask(
 					[target = a_target,
-				     node = a_entry.node]() {
+				     node   = a_entry.node]() {
 						if (node->m_parent &&
 					        node->m_parent != target)
 						{

.gitignore
@@ -361,3 +361,6 @@ FodyWeavers.xsd
 /x64_v143/common_1_5_97_vc14/ReleaseAVX2 MT
 /ImmersiveEquipmentDisplays/gitparams.h
 /x64_v143/common_1_5_97_vc14/Release MT
+/x64_v142/common_1_5_97_vc14/Release MD/common_1_5_97_vc14.lib
+/x64_v143/common_1_5_97_vc14/ReleaseAVX2 MD/common_1_5_97_vc14.lib
+/x64_v142/common_1_5_97_vc14/Release MT/common_1_5_97_vc14.lib

ImmersiveEquipmentDisplays.sln
@@ -26,7 +26,7 @@ Global
 		PreRelease MT|x64 = PreRelease MT|x64
 		PreRelease|x64 = PreRelease|x64
 		PreReleaseAVX2 MT|x64 = PreReleaseAVX2 MT|x64
-		Release DT|x64 = Release DT|x64
+		Release MD|x64 = Release MD|x64
 		Release MT|x64 = Release MT|x64
 		Release|x64 = Release|x64
 		ReleaseAVX2 MD|x64 = ReleaseAVX2 MD|x64
@@ -57,14 +57,14 @@ Global
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.PreRelease|x64.Build.0 = Release|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.PreReleaseAVX2 MT|x64.ActiveCfg = Release|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.PreReleaseAVX2 MT|x64.Build.0 = Release|x64
-		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.Release DT|x64.ActiveCfg = Release|x64
-		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.Release DT|x64.Build.0 = Release|x64
+		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.Release MD|x64.ActiveCfg = Release MD|x64
+		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.Release MD|x64.Build.0 = Release MD|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.Release MT|x64.ActiveCfg = Release MT|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.Release MT|x64.Build.0 = Release MT|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.Release|x64.ActiveCfg = Release|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.Release|x64.Build.0 = Release|x64
-		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.ReleaseAVX2 MD|x64.ActiveCfg = Release|x64
-		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.ReleaseAVX2 MD|x64.Build.0 = Release|x64
+		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.ReleaseAVX2 MD|x64.ActiveCfg = ReleaseAVX2 MD|x64
+		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.ReleaseAVX2 MD|x64.Build.0 = ReleaseAVX2 MD|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.ReleaseAVX2 MT W7|x64.ActiveCfg = Release|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.ReleaseAVX2 MT W7|x64.Build.0 = Release|x64
 		{A0AF6C5A-B0F8-4F7F-B622-535C5B9DCFF1}.ReleaseAVX2 MT|x64.ActiveCfg = ReleaseAVX2 MT|x64
@@ -93,14 +93,14 @@ Global
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.PreRelease|x64.Build.0 = PreRelease|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.PreReleaseAVX2 MT|x64.ActiveCfg = PreReleaseAVX2 MT|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.PreReleaseAVX2 MT|x64.Build.0 = PreReleaseAVX2 MT|x64
-		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.Release DT|x64.ActiveCfg = Release DT|x64
-		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.Release DT|x64.Build.0 = Release DT|x64
+		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.Release MD|x64.ActiveCfg = Release MD|x64
+		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.Release MD|x64.Build.0 = Release MD|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.Release MT|x64.ActiveCfg = Release MT|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.Release MT|x64.Build.0 = Release MT|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.Release|x64.ActiveCfg = Release MT|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.Release|x64.Build.0 = Release MT|x64
-		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.ReleaseAVX2 MD|x64.ActiveCfg = ReleaseAVX2 MT|x64
-		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.ReleaseAVX2 MD|x64.Build.0 = ReleaseAVX2 MT|x64
+		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.ReleaseAVX2 MD|x64.ActiveCfg = ReleaseAVX2 MD|x64
+		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.ReleaseAVX2 MD|x64.Build.0 = ReleaseAVX2 MD|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.ReleaseAVX2 MT W7|x64.ActiveCfg = ReleaseAVX2 MT W7|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.ReleaseAVX2 MT W7|x64.Build.0 = ReleaseAVX2 MT W7|x64
 		{5FD1C08D-DB80-480C-A1C6-F0920005CD13}.ReleaseAVX2 MT|x64.ActiveCfg = ReleaseAVX2 MT|x64
@@ -129,14 +129,14 @@ Global
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.PreRelease|x64.Build.0 = PreRelease|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.PreReleaseAVX2 MT|x64.ActiveCfg = PreReleaseAVX2 MT|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.PreReleaseAVX2 MT|x64.Build.0 = PreReleaseAVX2 MT|x64
-		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.Release DT|x64.ActiveCfg = Release DT|x64
-		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.Release DT|x64.Build.0 = Release DT|x64
+		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.Release MD|x64.ActiveCfg = Release MD|x64
+		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.Release MD|x64.Build.0 = Release MD|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.Release MT|x64.ActiveCfg = Release MT|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.Release MT|x64.Build.0 = Release MT|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.Release|x64.ActiveCfg = Release MT|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.Release|x64.Build.0 = Release MT|x64
-		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.ReleaseAVX2 MD|x64.ActiveCfg = ReleaseAVX2 MT|x64
-		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.ReleaseAVX2 MD|x64.Build.0 = ReleaseAVX2 MT|x64
+		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.ReleaseAVX2 MD|x64.ActiveCfg = ReleaseAVX2 MD|x64
+		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.ReleaseAVX2 MD|x64.Build.0 = ReleaseAVX2 MD|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.ReleaseAVX2 MT W7|x64.ActiveCfg = ReleaseAVX2 MT W7|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.ReleaseAVX2 MT W7|x64.Build.0 = ReleaseAVX2 MT W7|x64
 		{C59E5569-E3F3-4DA5-8313-12D90229B8C0}.ReleaseAVX2 MT|x64.ActiveCfg = ReleaseAVX2 MT|x64
@@ -165,14 +165,14 @@ Global
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.PreRelease|x64.Build.0 = PreRelease|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.PreReleaseAVX2 MT|x64.ActiveCfg = PreReleaseAVX2 MT|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.PreReleaseAVX2 MT|x64.Build.0 = PreReleaseAVX2 MT|x64
-		{472E19AB-DEF0-42DF-819B-18722E8DC822}.Release DT|x64.ActiveCfg = Release DT|x64
-		{472E19AB-DEF0-42DF-819B-18722E8DC822}.Release DT|x64.Build.0 = Release DT|x64
+		{472E19AB-DEF0-42DF-819B-18722E8DC822}.Release MD|x64.ActiveCfg = Release MD|x64
+		{472E19AB-DEF0-42DF-819B-18722E8DC822}.Release MD|x64.Build.0 = Release MD|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.Release MT|x64.ActiveCfg = Release MT|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.Release MT|x64.Build.0 = Release MT|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.Release|x64.ActiveCfg = Release MT|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.Release|x64.Build.0 = Release MT|x64
-		{472E19AB-DEF0-42DF-819B-18722E8DC822}.ReleaseAVX2 MD|x64.ActiveCfg = ReleaseAVX2 MT|x64
-		{472E19AB-DEF0-42DF-819B-18722E8DC822}.ReleaseAVX2 MD|x64.Build.0 = ReleaseAVX2 MT|x64
+		{472E19AB-DEF0-42DF-819B-18722E8DC822}.ReleaseAVX2 MD|x64.ActiveCfg = ReleaseAVX2 MD|x64
+		{472E19AB-DEF0-42DF-819B-18722E8DC822}.ReleaseAVX2 MD|x64.Build.0 = ReleaseAVX2 MD|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.ReleaseAVX2 MT W7|x64.ActiveCfg = ReleaseAVX2 MT W7|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.ReleaseAVX2 MT W7|x64.Build.0 = ReleaseAVX2 MT W7|x64
 		{472E19AB-DEF0-42DF-819B-18722E8DC822}.ReleaseAVX2 MT|x64.ActiveCfg = ReleaseAVX2 MT|x64
@@ -201,8 +201,8 @@ Global
 		{9F316E83-5AE5-4939-A723-305A94F48005}.PreRelease|x64.Build.0 = PreReleaseAVX2 MT|x64
 		{9F316E83-5AE5-4939-A723-305A94F48005}.PreReleaseAVX2 MT|x64.ActiveCfg = PreReleaseAVX2 MT|x64
 		{9F316E83-5AE5-4939-A723-305A94F48005}.PreReleaseAVX2 MT|x64.Build.0 = PreReleaseAVX2 MT|x64
-		{9F316E83-5AE5-4939-A723-305A94F48005}.Release DT|x64.ActiveCfg = Release MT|x64
-		{9F316E83-5AE5-4939-A723-305A94F48005}.Release DT|x64.Build.0 = Release MT|x64
+		{9F316E83-5AE5-4939-A723-305A94F48005}.Release MD|x64.ActiveCfg = Release MD|x64
+		{9F316E83-5AE5-4939-A723-305A94F48005}.Release MD|x64.Build.0 = Release MD|x64
 		{9F316E83-5AE5-4939-A723-305A94F48005}.Release MT|x64.ActiveCfg = Release MT|x64
 		{9F316E83-5AE5-4939-A723-305A94F48005}.Release MT|x64.Build.0 = Release MT|x64
 		{9F316E83-5AE5-4939-A723-305A94F48005}.Release|x64.ActiveCfg = Release MT|x64

ImmersiveEquipmentDisplays/IED/ConfigConditionsCommon.cpp
@@ -84,5 +84,17 @@ namespace IED
 
 			return false;
 		}
+
+		bool is_ammo_bolt(TESForm* a_form)
+		{
+			if (auto ammo = a_form->As<TESAmmo>())
+			{
+				return ammo->isBolt();
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 }

ImmersiveEquipmentDisplays/IED/ConfigConditionsCommon.h
@@ -58,7 +58,7 @@ namespace IED
 			bool                  a_left)
 		{
 			return a_left ?
-                       Data::ItemData::GetItemSlotLeftExtra(a_form) == a_slot :
+			           Data::ItemData::GetItemSlotLeftExtra(a_form) == a_slot :
                        Data::ItemData::GetItemSlotExtra(a_form) == a_slot;
 		}
 
@@ -141,6 +141,15 @@ namespace IED
 				}
 			}
 
+			if (a_match.flags.test(Tf::kExtraFlag1))
+			{
+				if (a_match.flags.test(Tf::kNegateMatch3) ==
+				    is_ammo_bolt(form))
+				{
+					return false;
+				}
+			}
+
 			a_post(form);
 
 			return true;
@@ -177,7 +186,7 @@ namespace IED
 					return false;
 				}
 			}
-			
+
 			if (a_match.flags.test(Tf::kExtraFlag2))
 			{
 				if (a_match.flags.test(Tf::kNegateMatch4) ==
@@ -276,7 +285,7 @@ namespace IED
 
 						if (a_match.flags.test(Tf::kNegateMatch2) ==
 						    (location ?
-                                 is_in_location(current, keyword, location) :
+						         is_in_location(current, keyword, location) :
                                  is_in_location(current, keyword)))
 						{
 							return false;
@@ -400,6 +409,8 @@ namespace IED
 				return false;
 			}
 		}
+
+		bool is_ammo_bolt(TESForm* a_form);
 
 	}
 }

ImmersiveEquipmentDisplays/IED/ConfigOverrideCustom.h
@@ -23,6 +23,7 @@ namespace IED
 			kDisableHavok         = 1u << 8,
 			kUseGroup             = 1u << 9,
 			kCheckFav             = 1u << 10,
+			kSelectInvRandom      = 1u << 11,
 		};
 
 		DEFINE_ENUM_CLASS_BITWISE(CustomFlags);

ImmersiveEquipmentDisplays/IED/ConfigOverrideEquipment.h
@@ -31,7 +31,7 @@ namespace IED
 
 			kMatchAll = kMatchEquipped | kMatchSlots,
 
-			// laying down (Furniture), loc child (Location), match parent (Worldspace), playable (Race)
+			// laying down (Furniture), loc child (Location), match parent (Worldspace), playable (Race), is bolt (Biped)
 			kExtraFlag1 = 1u << 11,
 
 			// match skin (Biped), is child (Race)

ImmersiveEquipmentDisplays/IED/ConfigOverrideNodeOverride.h
@@ -60,7 +60,7 @@ namespace IED
 			// ignore scb (Node)
 			kExtraFlag0 = 1u << 10,
 
-			// laying down (Furniture), is child (Location), match parent (Worldspace), playable (Race)
+			// laying down (Furniture), is child (Location), match parent (Worldspace), playable (Race), is bolt (Biped)
 			kExtraFlag1 = 1u << 11,
 
 			// match skin (Biped), is child (Race)
@@ -777,7 +777,7 @@ namespace IED
 				{
 					auto it = a_data.find(a_key);
 					return it != a_data.end() ?
-                               std::addressof(it->second) :
+					           std::addressof(it->second) :
                                nullptr;
 				}
 			}

commit e17b854bd5c3ec98f3e8c984438991e648883d4f

Gear positioning: fix and optimize 'adjusts from matched armors only' option Broken in 2ed4c70

ImmersiveEquipmentDisplays/IED/Controller/INodeOverride.cpp
@@ -79,15 +79,15 @@ namespace IED
 			}
 		}
 
-		auto data = a_params.get_item_data();
+		auto& data = a_params.get_item_data();
 
-		for (auto& e : *data)
+		for (auto& e : data)
 		{
 			if (IFormCommon::HasKeyword(e.second.item, a_keyword))
 			{
 				if (e.second.item->IsArmor())
 				{
-					e.second.matched = true;
+					a_params.set_matched_item(e.second.bip);
 				}
 
 				return true;
@@ -125,10 +125,10 @@ namespace IED
 			return { form, nullptr };
 		}
 
-		auto data = a_params.get_item_data();
+		auto& data = a_params.get_item_data();
 
-		auto it = data->find(a_form);
-		if (it != data->end())
+		auto it = data.find(a_form);
+		if (it != data.end())
 		{
 			return { it->second.item, std::addressof(*it) };
 		}
@@ -158,7 +158,7 @@ namespace IED
 
 		if (r.second && r.second->second.item->IsArmor())
 		{
-			r.second->second.matched = true;
+			a_params.set_matched_item(r.second->second.bip);
 		}
 
 		return true;
@@ -242,13 +242,13 @@ namespace IED
 
 				if (a_match.form.get_id())
 				{
-					auto data = a_params.get_item_data();
+					auto& data = a_params.get_item_data();
 
-					auto it = data->find(a_match.form.get_id());
+					auto it = data.find(a_match.form.get_id());
 
 					auto rv = a_match.flags.test(Data::NodeOverrideConditionFlags::kNegateMatch1);
 
-					if (it == data->end())
+					if (it == data.end())
 					{
 						return rv;
 					}
@@ -269,7 +269,10 @@ namespace IED
 						}
 					}
 
-					it->second.matched = !rv;
+					if (!rv)
+					{
+						a_params.set_matched_item(it->second.bip);
+					}
 
 					return !rv;
 				}
@@ -458,11 +461,11 @@ namespace IED
 					[&](TESForm* a_form) {
 						if (a_form->IsArmor())
 						{
-							auto data = a_params.get_item_data();
-							auto it   = data->find(a_form->formID);
-							if (it != data->end())
+							auto& data = a_params.get_item_data();
+							auto  it   = data.find(a_form->formID);
+							if (it != data.end())
 							{
-								it->second.matched = true;
+								a_params.set_matched_item(it->second.bip);
 							}
 						}
 					});
@@ -842,6 +845,8 @@ namespace IED
 
 		for (auto& e : a_data)
 		{
+			a_params.clear_matched_items();
+
 			if (run_matches(e, a_params))
 			{
 				if (e.offsetFlags.test(Data::NodeOverrideOffsetFlags::kIsGroup))
@@ -859,8 +864,6 @@ namespace IED
 				else
 				{
 					matched = true;
-
-					a_params.clear_matched_items();
 
 					if (e.offsetFlags.test_any(Data::NodeOverrideOffsetFlags::kAdjustFlags))
 					{
@@ -1018,7 +1021,7 @@ namespace IED
 	}
 
 	auto INodeOverride::nodeOverrideParams_t::get_item_data()
-		-> item_container_type*
+		-> item_container_type&
 	{
 		if (!itemData)
 		{
@@ -1063,7 +1066,7 @@ namespace IED
 			}
 		}
 
-		return itemData.get();
+		return *itemData;
 	}
 
 	float INodeOverride::nodeOverrideParams_t::get_weapon_adjust()

ImmersiveEquipmentDisplays/IED/Controller/INodeOverride.h
@@ -27,7 +27,6 @@ namespace IED
 			TESForm*     item;
 			BIPED_OBJECT bip{ BIPED_OBJECT::kNone };
 			float        weaponAdjust{ 0.0f };
-			bool         matched{ false };
 		};
 
 		struct nodeOverrideParamsArgs_t
@@ -48,6 +47,7 @@ namespace IED
 			stl::optional<float>                 weightAdjust;
 			std::unique_ptr<item_container_type> itemData;
 			stl::optional<bool>                  bipedHasArmor;
+			std::uint64_t                        matchedSlotFlags{ 0 };
 
 			auto get_biped_has_armor()
 			{
@@ -59,7 +59,7 @@ namespace IED
 				return *bipedHasArmor;
 			}
 
-			item_container_type* get_item_data();
+			item_container_type& get_item_data();
 
 			float get_weapon_adjust();
 
@@ -73,15 +73,14 @@ namespace IED
 				return *weightAdjust;
 			}
 
-			void clear_matched_items() noexcept
+			inline constexpr void clear_matched_items() noexcept
 			{
-				if (itemData)
-				{
-					for (auto& e : *itemData)
-					{
-						e.second.matched = false;
-					}
-				}
+				matchedSlotFlags = 0;
+			}
+
+			inline constexpr void set_matched_item(BIPED_OBJECT a_object) noexcept
+			{
+				matchedSlotFlags |= 1ui64 << stl::underlying(a_object);
 			}
 
 			float get_matched_weapon_adjust() const noexcept
@@ -92,7 +91,7 @@ namespace IED
 				{
 					for (auto& e : *itemData)
 					{
-						if (e.second.matched)
+						if (matchedSlotFlags & (1ui64 << stl::underlying(e.second.bip)))
 						{
 							result += e.second.weaponAdjust;
 						}
@@ -103,7 +102,7 @@ namespace IED
 			}
 
 			template <class Tf>
-			bool equipped_armor_visitor(
+			constexpr bool equipped_armor_visitor(
 				Tf a_func)
 			{
 				auto bip = get_biped();
@@ -127,12 +126,14 @@ namespace IED
 
 					if (e.item &&
 					    e.item != e.addon &&
-					    e.item != skin &&
-					    e.item->IsArmor())
+					    e.item != skin)
 					{
-						if (a_func(e.item))
+						if (auto armor = e.item->As<TESObjectARMO>())
 						{
-							return true;
+							if (a_func(armor) == true)
+							{
+								return true;
+							}
 						}
 					}
 				}
@@ -141,12 +142,17 @@ namespace IED
 			}
 
 		private:
-			inline static constexpr bool is_av_ignored_slot(
+			inline constexpr bool is_av_ignored_slot(
 				BIPED_OBJECT a_slot) noexcept
 			{
+				if (a_slot == get_shield_slot())
+				{
+					return true;
+				}
+
 				switch (a_slot)
 				{
-				case BIPED_OBJECT::kShield:
+					// ??
 				case BIPED_OBJECT::kDecapitateHead:
 				case BIPED_OBJECT::kDecapitate:
 				case BIPED_OBJECT::kFX01: