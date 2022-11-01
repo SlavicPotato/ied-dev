b6e754f64de640c1d7c656105489ed34c25da21a
SlavicPotato <sp@eth.si>
2022-11-01 20:16:02 +0100

    constexpr these

--- ImmersiveEquipmentDisplays/IED/Data.h
+++ ImmersiveEquipmentDisplays/IED/Data.h
@@ -747,12 +747,194 @@ namespace IED
 
 			static IData m_Instance;
 		};
+				
+		inline constexpr const char* GetSlotName(ObjectSlot a_slot)
+		{
+			switch (a_slot)
+			{
+			case ObjectSlot::k1HSword:
+				return "One-Handed Sword";
+			case ObjectSlot::k1HSwordLeft:
+				return "One-Handed Sword Left";
+			case ObjectSlot::k1HAxe:
+				return "One-Handed Axe";
+			case ObjectSlot::k1HAxeLeft:
+				return "One-Handed Axe Left";
+			case ObjectSlot::k2HSword:
+				return "Two-Handed Sword";
+			case ObjectSlot::k2HSwordLeft:
+				return "Two-Handed Sword Left";
+			case ObjectSlot::k2HAxe:
+				return "Two-Handed Axe";
+			case ObjectSlot::k2HAxeLeft:
+				return "Two-Handed Axe Left";
+			case ObjectSlot::kMace:
+				return "Mace";
+			case ObjectSlot::kMaceLeft:
+				return "Mace Left";
+			case ObjectSlot::kDagger:
+				return "Dagger";
+			case ObjectSlot::kDaggerLeft:
+				return "Dagger Left";
+			case ObjectSlot::kStaff:
+				return "Staff";
+			case ObjectSlot::kStaffLeft:
+				return "Staff Left";
+			case ObjectSlot::kBow:
+				return "Bow";
+			case ObjectSlot::kCrossBow:
+				return "Crossbow";
+			case ObjectSlot::kShield:
+				return "Shield";
+			case ObjectSlot::kTorch:
+				return "Torch";
+			case ObjectSlot::kAmmo:
+				return "Ammo";
+			default:
+				return nullptr;
+			}
+		}
 
-		const char* GetSlotName(ObjectSlot a_slot);
-		const char* GetSlotName(ObjectSlotExtra a_slot);
-		const char* GetObjectTypeName(ObjectType a_type);
-		const char* GetObjectTypeName(ObjectTypeExtra a_type);
-		const char* GetConfigClassName(ConfigClass a_class);
+		inline constexpr const char* GetSlotName(ObjectSlotExtra a_slot)
+		{
+			switch (a_slot)
+			{
+			case ObjectSlotExtra::k1HSword:
+				return "One-Handed Sword";
+			case ObjectSlotExtra::k1HSwordLeft:
+				return "One-Handed Sword Left";
+			case ObjectSlotExtra::k1HAxe:
+				return "One-Handed Axe";
+			case ObjectSlotExtra::k1HAxeLeft:
+				return "One-Handed Axe Left";
+			case ObjectSlotExtra::k2HSword:
+				return "Two-Handed Sword";
+			case ObjectSlotExtra::k2HSwordLeft:
+				return "Two-Handed Sword Left";
+			case ObjectSlotExtra::k2HAxe:
+				return "Two-Handed Axe";
+			case ObjectSlotExtra::k2HAxeLeft:
+				return "Two-Handed Axe Left";
+			case ObjectSlotExtra::kMace:
+				return "Mace";
+			case ObjectSlotExtra::kMaceLeft:
+				return "Mace Left";
+			case ObjectSlotExtra::kDagger:
+				return "Dagger";
+			case ObjectSlotExtra::kDaggerLeft:
+				return "Dagger Left";
+			case ObjectSlotExtra::kStaff:
+				return "Staff";
+			case ObjectSlotExtra::kStaffLeft:
+				return "Staff Left";
+			case ObjectSlotExtra::kBow:
+				return "Bow";
+			case ObjectSlotExtra::kCrossBow:
+				return "Crossbow";
+			case ObjectSlotExtra::kShield:
+				return "Shield";
+			case ObjectSlotExtra::kTorch:
+				return "Torch";
+			case ObjectSlotExtra::kArmor:
+				return "Armor";
+			case ObjectSlotExtra::kAmmo:
+				return "Ammo";
+			case ObjectSlotExtra::kSpell:
+				return "Spell";
+			case ObjectSlotExtra::kSpellLeft:
+				return "Spell Left";
+			default:
+				return nullptr;
+			}
+		}
+
+		inline constexpr const char* GetObjectTypeName(ObjectType a_type)
+		{
+			switch (a_type)
+			{
+			case ObjectType::k1HSword:
+				return "One-Handed Sword";
+			case ObjectType::kDagger:
+				return "Dagger";
+			case ObjectType::k1HAxe:
+				return "One-Handed Axe";
+			case ObjectType::kMace:
+				return "Mace";
+			case ObjectType::kStaff:
+				return "Staff";
+			case ObjectType::k2HSword:
+				return "Two-Handed Sword";
+			case ObjectType::k2HAxe:
+				return "Two-Handed Axe";
+			case ObjectType::kBow:
+				return "Bow";
+			case ObjectType::kCrossBow:
+				return "Crossbow";
+			case ObjectType::kShield:
+				return "Shield";
+			case ObjectType::kTorch:
+				return "Torch";
+			case ObjectType::kAmmo:
+				return "Ammo";
+			default:
+				return nullptr;
+			}
+		}
+
+		inline constexpr const char* GetObjectTypeName(ObjectTypeExtra a_type)
+		{
+			switch (a_type)
+			{
+			case ObjectTypeExtra::k1HSword:
+				return "One-Handed Sword";
+			case ObjectTypeExtra::kDagger:
+				return "Dagger";
+			case ObjectTypeExtra::k1HAxe:
+				return "One-Handed Axe";
+			case ObjectTypeExtra::kMace:
+				return "Mace";
+			case ObjectTypeExtra::kStaff:
+				return "Staff";
+			case ObjectTypeExtra::k2HSword:
+				return "Two-Handed Sword";
+			case ObjectTypeExtra::k2HAxe:
+				return "Two-Handed Axe";
+			case ObjectTypeExtra::kBow:
+				return "Bow";
+			case ObjectTypeExtra::kCrossBow:
+				return "Crossbow";
+			case ObjectTypeExtra::kShield:
+				return "Shield";
+			case ObjectTypeExtra::kTorch:
+				return "Torch";
+			case ObjectTypeExtra::kArmor:
+				return "Armor";
+			case ObjectTypeExtra::kAmmo:
+				return "Ammo";
+			case ObjectTypeExtra::kSpell:
+				return "Spell";
+			default:
+				return nullptr;
+			}
+		}
+
+		inline constexpr const char* GetConfigClassName(ConfigClass a_class)
+		{
+			switch (a_class)
+			{
+			case ConfigClass::Actor:
+				return "Actor";
+			case ConfigClass::NPC:
+				return "NPC";
+			case ConfigClass::Race:
+				return "Race";
+			case ConfigClass::Global:
+				return "Global";
+			default:
+				return nullptr;
+			}
+		}
+
 
 		std::unique_ptr<configSlotHolder_t::data_type>
 			CreateDefaultSlotConfig(ObjectSlot a_slot);
